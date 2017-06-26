edb4dcb7df8386d43757b2a1b762b4591e3a0f52
Raymond Blum <raygeeknyc@gmail.com>
2017-06-26 09:10:30 -0400

    Fix interpreter for extended test

--- setup_auth.sh
+++ setup_auth.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 export GOOGLE_APPLICATION_CREDENTIALS=../paidtech-07060b047c89.json
 if [[ ! -r "$GOOGLE_APPLICATION_CREDENTIALS" ]]; then
   gcloud auth application-default login
