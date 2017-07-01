73f3c466d3f49843c437b37e78fd5e81905cebb3
Raymond Blum <raygeeknyc@gmail.com>
2017-07-01 11:34:10 -0400

    Added better messages to setup_auth

--- setup_auth.sh
+++ setup_auth.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
-export GOOGLE_APPLICATION_CREDENTIALS=../paidtech-07060b047c89.json
+echo "authenticating"
+GOOGLE_APPLICATION_CREDENTIALS=../paidtech-07060b047c89.json
 if [[ ! -r "$GOOGLE_APPLICATION_CREDENTIALS" ]]; then
+  echo "falling back to interactive authentication"
   gcloud auth application-default login
+else
+  export GOOGLE_APPLICATION_CREDENTIALS
 fi
+echo "authenticated"
