888507a043d1f8a58843d629f245bbc25f168653
Raymond Blum <raygeeknyc@gmail.com>
2017-07-17 06:56:58 -0400

    Added tests to see why git pull is not working

--- setup_auth.sh
+++ setup_auth.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
-echo "updating"
-git pull
+ping -c 1 www.github.com 2>&1
+rc=$?
+if [[ $rc eq 0 ]];then
+   echo "updating"
+   git pull 2>&1
+else
+   echo "not updating" >&2
+fi
 echo "authenticating"
 GOOGLE_APPLICATION_CREDENTIALS=../paidtech-07060b047c89.json
 if [[ ! -r "$GOOGLE_APPLICATION_CREDENTIALS" ]]; then
