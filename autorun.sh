2c0e876a90e5cadb53c3d34f658ca70d7112b056
Raymond Blum <raygeeknyc@users.noreply.github.com>
2017-07-17 14:22:56 -0400

    Update autorun.sh
    moved connectivity test

--- autorun.sh
+++ autorun.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
+echo "Waiting for connectivity"
+rc=255
+while [[ $rc -ne 0 ]];do
+  sleep 1
+  ping -c 1 www.github.com 2>&1
+  rc=$?
+done
 ./setup_auth.sh 2>&1
 nohup python ohgee.py &
