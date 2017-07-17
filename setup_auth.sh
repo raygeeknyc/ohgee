f8a717f888d7810a926213861f599c53c065c65a
Raymond Blum <raygeeknyc@users.noreply.github.com>
2017-07-17 14:24:12 -0400

    Update setup_auth.sh
    cleanup followup

--- setup_auth.sh
+++ setup_auth.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
-echo "waiting for connectivity"
-echo "updating"
-git pull 2>&1
 echo "authenticating"
 GOOGLE_APPLICATION_CREDENTIALS=../paidtech-07060b047c89.json
 if [[ ! -r "$GOOGLE_APPLICATION_CREDENTIALS" ]]; then
