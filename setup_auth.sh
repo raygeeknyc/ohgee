03af1bf2dd5b23ee9da3bdfd155c909891949a1b
Raymond Blum <raygeeknyc@users.noreply.github.com>
2017-07-17 14:22:01 -0400

    Update setup_auth.sh
    move connectivity to the driver script

--- setup_auth.sh
+++ setup_auth.sh
@@ -1,17 +1,7 @@
 #!/bin/bash
 echo "waiting for connectivity"
-rc=255
-while [[ $rc -ne 0 ]];do
-  sleep 1
-  ping -c 1 www.github.com 2>&1
-  rc=$?
-done
-if [[ $rc -eq 0 ]];then
-   echo "updating"
-   git pull 2>&1
-else
-   echo "not updating" >&2
-fi
+echo "updating"
+git pull 2>&1
 echo "authenticating"
 GOOGLE_APPLICATION_CREDENTIALS=../paidtech-07060b047c89.json
 if [[ ! -r "$GOOGLE_APPLICATION_CREDENTIALS" ]]; then
