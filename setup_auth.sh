b38e1f71d9c5395e8ac388e3950e2d4a7812e39c
Raymond Blum <raygeeknyc@gmail.com>
2017-07-17 07:03:17 -0400

    Add wait for network connectivity

--- setup_auth.sh
+++ setup_auth.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
-ping -c 1 www.github.com 2>&1
-rc=$?
-if [[ $rc eq 0 ]];then
+echo "waiting for connectivity"
+rc=255
+while [[ $rc -ne 0 ]];do
+  sleep 1
+  ping -c 1 www.github.com 2>&1
+  rc=$?
+done
+if [[ $rc -eq 0 ]];then
    echo "updating"
    git pull 2>&1
 else
