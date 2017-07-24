18d84e2e6648670efbb0e9525ebb0517ed434d03
Raymond Blum <raygeeknyc@gmail.com>
2017-07-24 06:56:34 -0400

    Fixed messaging of install

--- autorun.sh
+++ autorun.sh
@@ -11,14 +11,13 @@ while [ $(date +%s) -lt $endTime ]; do  # Loop until interval has elapsed.
     break
   fi
 done
-pico2wave -l en-US --wave "/tmp/ohgee_help.wav" "installing";aplay "/tmp/ohgee_help.wav"
 if [[ $rc -ne 0 ]]; then
   pico2wave -l en-US --wave "/tmp/ohgee_help.wav" "help!  no network";aplay "/tmp/ohgee_help.wav"
   echo "sudo reboot" | at now + 1 hour
   pico2wave -l en-US --wave "/tmp/ohgee_help.wav" "Reboot in one hour";aplay "/tmp/ohgee_help.wav"
   exit 255
 fi
-echo "updating"
+pico2wave -l en-US --wave "/tmp/ohgee_help.wav" "updating";aplay "/tmp/ohgee_help.wav"
 ./update.sh
 if [[ -z "$DISPLAY" ]];then
   export DISPLAY=":0.0"
