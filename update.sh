5c516bc4b33d9976f065042d38e5c5dfe26594b2
Raymond Blum <raygeeknyc@gmail.com>
2017-08-29 23:45:29 -0400

    Added message confirming update

--- update.sh
+++ update.sh
@@ -2,3 +2,4 @@
 git pull
 #sudo apt-get -y --force-yes install python-imaging-tk
 #sudo pip install --upgrade google-api-python-client
+pico2wave -l en-US --wave "/tmp/ohgee_update.wav" "update done";aplay "/tmp/ohgee_update.wav"
