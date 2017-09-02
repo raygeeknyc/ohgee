fe04aafbbc5c79636749ba80b299cece2f26c7ad
Raymond Blum <raygeeknyc@gmail.com>
2017-09-02 20:59:13 +0000

    Add simple image display test for new displays

---
+++ displaytest.py
@@ -0,0 +1,30 @@
+import logging
+
+# reorder as appropriate
+_DEBUG = logging.DEBUG
+_DEBUG = logging.INFO
+
+import Tkinter as tk
+import PIL
+from PIL import ImageTk, Image
+
+import time
+import io
+import sys
+
+def maintainDisplay(root_window):
+    image = Image.open('test.jpg')
+    tk_image = ImageTk.PhotoImage(image)
+    tk.Label(root_window, image=tk_image).pack()
+    root_window.mainloop()
+    time.sleep(3)
+    root_window.quit()
+    
+if __name__ == '__main__':
+    root = tk.Tk()
+    root.wm_attributes('-type', 'splash')
+    root.geometry("%dx%d+%d+%d" % (root.winfo_screenwidth(), root.winfo_screenheight(), 0, 0))
+    print("%dx%d" % (root.winfo_screenwidth(), root.winfo_screenheight()))
+
+    maintainDisplay(root)
+    sys.exit(0)
