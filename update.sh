74619fcf82707a0cef71883eed3f512822f498aa
Raymond Blum <rsygeeknyc@gmail.com>
2017-09-05 07:59:36 -0400

    Remove temp patch for display rotate

--- update.sh
+++ update.sh
@@ -3,6 +3,4 @@ git pull
 #sudo apt-get -y --force-yes install python-imaging-tk
 #sudo pip install --upgrade google-api-python-client
 pico2wave -l en-US --wave "/tmp/ohgee_update.wav" "updated";aplay "/tmp/ohgee_update.wav"
-sudo echo "display_rotate=2" >> /boot/config.txt
-pico2wave -l en-US --wave "/tmp/ohgee_update.wav" "Display rotate installed";aplay "/tmp/ohgee_update.wav"
 
