c6f368c5edf327bd169e7b5765b973ffbf95d73b
Raymond Blum <raygeeknyc@gmail.com>
2017-12-03 16:46:38 -0500

    add some text analysis to the client

--- ohgee.py
+++ ohgee.py
@@ -138,6 +138,9 @@ def receiveLanguageResults(nl_results, search_queue):
             phrase = nl_results.recv()
             text, tokens, entities, sentiment, decorated_noun = phrase
             logging.debug("Got spoken phrase {}".format(text))
+            logging.info("Text: "".format(text)
+            logging.info("Tokens: "".format(tokens)
+            logging.info("Entities: "".format(entities)
             if speechanalyzer.isGood(sentiment):
                 showGoodMood(sentiment.score)
             elif speechanalyzer.isBad(sentiment):
