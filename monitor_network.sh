0755173441b9646faf90722a41af2691dc2d2506
Raymond Blum <raygeeknyc@gmail.com>
2018-06-11 21:07:19 -0400

    Fixed detection of pending jobs - the current job is found

--- monitor_network.sh
+++ monitor_network.sh
@@ -4,19 +4,21 @@ SCRIPT="$( basename $0 )"
 ping -c 1 www.google.com  1>/dev/null 2>&1
 rc=$?
 if [[ $rc -ne 0 ]]; then
+  logger "$0 ping failed"
   /usr/bin/pico2wave -l en-US --wave "/tmp/ohgee_help.wav" "help!  no network";/usr/bin/aplay "/tmp/ohgee_help.wav"
   /usr/bin/pico2wave -l en-US --wave "/tmp/ohgee_help.wav" "Rebooting";/usr/bin/aplay "/tmp/ohgee_help.wav"
   logger "$0 is rebooting"
   sudo reboot
   exit 255
 else
+  jobcount=0
   for job in $(at -l | awk '{print $1}'); do
     pending="$(at -c ${job} | grep ${SCRIPT})"
     if [[ -n "${pending}" ]]; then
-      break
+      jobcount=$(( jobcount + 1 ))
     fi
   done
-  if [[ -z "${pending}" ]]; then
+  if [[ ${jobcount} -le 1 ]]; then
     echo "${DIR}/${SCRIPT}" | at now + 2 minutes
   fi
   exit 0
