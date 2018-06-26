d2b31c826a5971c8ca2950d2b17256bd265f94d0
Raymond Blum <raygeeknyc@gmail.com>
2018-06-26 09:37:27 -0400

    Add commands to turn the small TFT backlight on and off

--- screen_sleep.sh
+++ screen_sleep.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 # Use whatever means to turn the screen off
 vcgencmd display_power 0
+gpio -g write 23 0
 exit 0
--- screen_wake.sh
+++ screen_wake.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 # Use whatever means to turn the screen on
 vcgencmd display_power 1
+gpio -g write 23 1
 exit 0
