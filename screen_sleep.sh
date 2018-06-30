418f4c385d334bb928e1562a651a198f3abb0b61
Raymond Blum <raygeeknyc@users.noreply.github.com>
2018-06-30 13:32:26 -0400

    Update screen_sleep.sh
    add audio confirmation of display sleep

--- screen_sleep.sh
+++ screen_sleep.sh
@@ -2,4 +2,5 @@
 # Use whatever means to turn the screen off
 vcgencmd display_power 0
 gpio -g write 23 0
+pico2wave -l en-US --wave "/tmp/ohgee_online.wav" "Sleeping";aplay "/tmp/ohgee_online.wav"
 exit 0
