20f87cf110cee10585d5b02da541c1ee7ac7d4be
Raymond Blum <raygeeknyc@users.noreply.github.com>
2018-06-30 13:33:03 -0400

    Update screen_wake.sh
    Add audio confirmation of display wake

--- screen_wake.sh
+++ screen_wake.sh
@@ -2,4 +2,5 @@
 # Use whatever means to turn the screen on
 vcgencmd display_power 1
 gpio -g write 23 1
+pico2wave -l en-US --wave "/tmp/ohgee_online.wav" "Waking";aplay "/tmp/ohgee_online.wav"
 exit 0
