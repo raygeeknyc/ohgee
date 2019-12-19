9b2839e9d6dd8db80a3c92440bf84954872e402a
Raymond Blum <raygeeknyc@users.noreply.github.com>
2019-12-19 06:56:54 -0500

    Update setaudio.sh

--- setaudio.sh
+++ setaudio.sh
@@ -11,8 +11,8 @@ gpio -g mode 19 Alt5
 #sleep 2
 #pacmd set-default-sink bluez_sink.11_58_02_B4_02_50
 #pactl -- set-sink-volume 1 40%
-# Lower the volume on the default audio output device
+# Set the volume on the default audio output device
 VOLUME_DEVICE=$(amixer controls | grep 'Playback Volume' | sed 's/numid=//' | sed 's/,.*//')
 if [[ -n "$VOLUME_DEVICE" ]]; then
-  sudo amixer cset numid=$VOLUME_DEVICE 95%
+  sudo amixer cset numid=$VOLUME_DEVICE 100%
 fi
