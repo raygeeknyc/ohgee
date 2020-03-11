23e45a04edc0a8515ea13bf4210b8c92084afe6c
Raymond Blum <raygeeknyc@users.noreply.github.com>
2020-03-11 14:28:52 -0400

    Update phraseresponder.py
    add reboot responses

--- phraseresponder.py
+++ phraseresponder.py
@@ -9,7 +9,7 @@ from datetime import datetime
 from google.cloud.language.entity import EntityType
 
 REBOOT_PROMPTS = (["og", "please", "reboot"], ["oh", "please", "reboot"], ["o", "please", "reboot"])
-REBOOT_RESPONSES = (["rebooting"], ["I'm", "rebooting"])
+REBOOT_RESPONSES = (["rebooting"], ["I'm", "rebooting"],  ["Okay", "", "I'm", "on", "it"])
 
 POP_1_PROMPTS = (["who", "is", "the", "man"], ["who", "would", "risk", "his", "neck"], ["his", "neck", "for", "his", "brother", "man"], ["the", "cat", "that", "won't", "cop", "out", "when"], ["danger", "all", "about"] )
 POP_1_RESPONSES = (["SHAFT"], ["that's", "shaft"], ["john", "shaft"])
