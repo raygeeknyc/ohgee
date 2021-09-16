0fb41e0a90dc4ad66ce88024f330a8d39edeb0b4
Raymond Blum <raygeeknyc@users.noreply.github.com>
2021-09-16 18:56:21 -0400

    Update setup_auth.sh

--- setup_auth.sh
+++ setup_auth.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
-credentials_file=`ls ../*.json | head -1`
-GOOGLE_APPLICATION_CREDENTIALS=${credentials_file}
-if [[ ! -r "${credentials_file}" ]]; then
-  echo "falling back to interactive authentication" >&2
-  gcloud auth application-default login
-else
-  export GOOGLE_APPLICATION_CREDENTIALS=${credentials_file}
-  export GOOGLE_CLOUD_PROJECT=ohgee-176600
-fi
+#
+# we now use application default credentials
+#credentials_file=`ls ../*.json | head -1`
+#GOOGLE_APPLICATION_CREDENTIALS=${credentials_file}
+#if [[ ! -r "${credentials_file}" ]]; then
+#  echo "falling back to interactive authentication" >&2
+#  gcloud auth application-default login
+#else
+#  export GOOGLE_APPLICATION_CREDENTIALS=${credentials_file}
+#  export GOOGLE_CLOUD_PROJECT=ohgee-176600
+#fi
+exit 0
